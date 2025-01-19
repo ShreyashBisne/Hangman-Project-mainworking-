eb51cde98ad35d5fe803ffd9c4d44b2fa1a6845e
ShreyashBisne <144591684+ShreyashBisne@users.noreply.github.com>
2025-01-19 14:23:08 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,12 +1,7 @@
-import random
-def chooseword():
-    words = ["shreyash", "aryan", "aman", "parth", "rehan"]
-    return random.choice(words)
-
-
+#This function displays the state of the word to be guessed.
 def displayword(word, guessedletters):
     return ' '.join([letter if letter in guessedletters else '_' for letter in word])
-    
+    #This is the main function for the game.
 def hangman():
     print("Welcome to the game!!!")
     guessedletters = set()
@@ -17,17 +12,17 @@ def hangman():
         print("Remaining chances : ", chances)
         print(f"Guessed Letters : {' '.join(sorted(guessedletters)) if guessedletters else 'None'}")
         
-        guess = input('Enter the next guess : ').lower()
+        guess = input('Enter the next guess : ').lower() #Makes all guesses in lower.
         
-        if len(guess) != 1 or not guess.isalpha() :
-            print("The guess was not appropriate")
+        if len(guess) != 1 or not guess.isalpha() : #Checks for valid inputs.
+            print("The guess was not appropriate. Please enter a single letter as your guess.")
             continue
 
-        if guess in guessedletters :
-            print("You Dumb")
+        if guess in guessedletters : #Checks whether the letter has already been guessed.
+            print("You have already guessed this letter please try again")
             continue
 
-        guessedletters.add(guess)
+        guessedletters.add(guess) #Adds the letter to the set of guessed letters.
 
         if guess in wordchosen:
             print("Good job! You guessed a letter correctly.")
@@ -36,11 +31,11 @@ def hangman():
                 break
 
         else:
-            print("Oops! That letter is not in the word.")
+            print("Oops! That letter is not in the word. Try Again")
             chances-=1
 
     if chances == 0 :
-        print("gameover")
+        print("Gameover. You have lost the game. The word was", wordchosen, ". Better luck next time.")
 
     
     
